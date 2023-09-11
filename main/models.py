d515da5557e9cbbd113761fb21c36a2dc8c59e90
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-09-11 10:54:11 +0700

    Updating models

--- main/models.py
+++ main/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    ammount = models.IntegerField()
     description = models.TextField()
+    effect = models.CharField(max_length=100,default='')
+    category = models.CharField(max_length=100,default='')
