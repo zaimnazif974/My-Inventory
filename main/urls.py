0705b2fb59273bb323d3d6c55f297f2a4972a87c
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-11-22 06:38:04 +0700

    Add(fetchFlutter)

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_item, show_xml, show_xml_by_id, show_json,show_json_by_id,register,login_user, logout_user 
-from main.views import edit_item, delete_item, get_items_json, add_item_ajax
+from main.views import edit_item, delete_item, get_items_json, add_item_ajax,create_product_flutter
 app_name = 'main'
 
 urlpatterns = [
@@ -17,4 +17,5 @@ urlpatterns = [
     path('delete/<int:id>', delete_item, name='delete_item'),
     path('get-item/', get_items_json, name='get_items_json'),
     path('create-ajax/', add_item_ajax, name='add_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -12,6 +12,8 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import datetime
+import json
+from django.http import JsonResponse
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -134,4 +136,25 @@ def add_item_ajax(request):
 
     return HttpResponseNotFound()
 
-#just a minor change
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Item.objects.create(
+            user = request.user,
+            name = data["name"],
+            ammount = int(data["amount"]),
+            description = data["description"],
+            effect = data["effect"],
+            date_added = data["dateAdded"],
+            category = data["category"],
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
